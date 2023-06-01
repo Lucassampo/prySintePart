25827040936ef76b80f42978d90178e3dc55db3e
sampolucas88@gmail.com <sampolucas88@gmail.com>
2023-06-01 15:55:10 +0200

    01/06/23

---
+++ SintePart/frmAcceso.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace SintePart
+{
+    partial class frmAcceso
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblAccesoSintepart = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblAccesoSintepart
+            // 
+            this.lblAccesoSintepart.AutoSize = true;
+            this.lblAccesoSintepart.Font = new System.Drawing.Font("Microsoft Sans Serif", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAccesoSintepart.Location = new System.Drawing.Point(44, 178);
+            this.lblAccesoSintepart.Name = "lblAccesoSintepart";
+            this.lblAccesoSintepart.Size = new System.Drawing.Size(479, 39);
+            this.lblAccesoSintepart.TabIndex = 0;
+            this.lblAccesoSintepart.Text = "Has accedido a SINTEPART";
+            // 
+            // frmAcceso
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(570, 450);
+            this.Controls.Add(this.lblAccesoSintepart);
+            this.Name = "frmAcceso";
+            this.Text = "Acceso";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblAccesoSintepart;
+    }
+}
--- SintePart/frmLogin.cs
+++ SintePart/frmLogin.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel;
+using System.ComponentModel.Design;
 using System.Data;
+using System.Diagnostics.Eventing.Reader;
 using System.Drawing;
 using System.Linq;
+using System.Runtime.Remoting.Messaging;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
@@ -12,6 +15,7 @@ namespace SintePart
 {
     public partial class frmLogin : Form
     {
+        int varintentos;
         public frmLogin()
         {
             InitializeComponent();
@@ -24,8 +28,8 @@ namespace SintePart
 
         private void cmdCancelar_Click(object sender, EventArgs e)
         {
-            this.Close();   
-    }
+            this.Close();
+        }
 
         private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
         {
@@ -39,17 +43,70 @@ namespace SintePart
 
         private void txtUsuario_TextChanged(object sender, EventArgs e)
         {
-            
+
         }
 
         private void cmdAceptar_Click(object sender, EventArgs e)
         {
-            if (txtUsuario.Text == "Adm" && txtContraseña.Text == "@1a") ;
+            if (txtUsuario.Text == "Adm" && txtContraseña.Text == "@1a") 
             {
-
-
-
+                if (cmbModulo.Text == "ADM" || cmbModulo.Text == "COM" || cmbModulo.Text == "VTA") 
+                {
+                    this.Hide();
+                    frmAcceso acceso = new frmAcceso();
+                    acceso.ShowDialog();
+                    varintentos = 0;
+                }
+                else
+                {
+                    MessageBox.Show("Usuario y/o contraseñas incorrectos");
+                    varintentos++;
+                }
+                if (txtUsuario.Text =="Jhon" && txtContraseña.Text =="*2b")
+                {
+                    if (cmbModulo.Text == "SIST")
+                    {
+                        this.Hide();
+                        frmAcceso acceso = new frmAcceso();
+                        acceso.ShowDialog();
+                        varintentos = 0;
+                    }
+                    else
+                    {
+                        MessageBox.Show("Usuario y/o contreseñas incorrectos");
+                        varintentos++;
+                    }
+                    if (txtUsuario.Text == "Ceci" && txtContraseña.Text =="*@3c")
+                    { 
+                      if(cmbModulo.Text == "AMD" || cmbModulo.Text =="VTA")
+                      {
+                            this.Hide();
+                            frmAcceso acceso=new frmAcceso();
+                            acceso.ShowDialog();
+                            varintentos = 0;
+                      }
+                      if(txtUsuario.Text =="God" && txtContraseña.Text =="*@#4d")
+                        {
+                            if(cmbModulo.Text == "AMD" || cmbModulo.Text == "SIST" ||cmbModulo.Text == "VTA" ||cmbModulo.Text =="COM")
+                            {
+                                this.Hide();
+                                frmAcceso acceso = new frmAcceso();
+                                acceso.ShowDialog();
+                                varintentos = 0;
+                                    
+                            }
+                            else
+                            {
+                                MessageBox.Show("Usuario y/o contraseñas incorrectas");
+                            }
+                        }
+                    }
+                }
             }
         }
-    }
+        
+        
+        
+        }
+    
 }
